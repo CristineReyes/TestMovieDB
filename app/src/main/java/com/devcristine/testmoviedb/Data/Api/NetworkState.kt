9cebbf3d533e4adb1a753a28806e7a7da13d2759
Cristine Aguirre <33470081+CristineReyes@users.noreply.github.com>
2021-09-26 07:50:53 -0500

    Initial commit

---
+++ app/src/main/java/com/devcristine/testmoviedb/Data/Api/NetworkState.kt
@@ -0,0 +1,17 @@
+package com.devcristine.testmoviedb.Data.Api
+
+
+/**
+ * Created by Cristine R.M. on 26/09/2021.
+ * DevCristineAguirre
+ */
+
+enum class Status{
+    RUNNING,
+    SUCCESS,
+    FAILED
+}
+
+
+class NetworkState {
+}
---
+++ app/src/main/java/com/devcristine/testmoviedb/Details/DetailsOne.kt
@@ -0,0 +1,11 @@
+package com.devcristine.testmoviedb.Details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_one)
+    }
+}
